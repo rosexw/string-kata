5a59bdabff4442c06620ec1cc6ee935a54975aba
Rose Wan <rwan@mail.com>
2017-07-17 23:40:49 +0000

    refactor requirement 3

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,9 @@
 module StringCalculator
 
   def self.add(string)
-    unless string.include? ","
-      string.to_i
-    else
+    return string.to_i unless string.include? ","
       a, b = string.split(",")
       a.to_i + b.to_i
-    end
   end
 
   # ...and here when the above becomes too complex.
